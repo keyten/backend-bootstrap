260df0ec5d5dd0e976dc3decea8c29cd110d060e
Keyten <ikeyten@gmail.com>
2021-07-24 16:02:21 +1000

    Fix config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,8 +5,9 @@ import env from './env';
 
 const configPath = path.join(__dirname, `../../config/${env}.yml`);
 const configText = fs.readFileSync(configPath, {encoding: 'utf-8'});
-export default yaml.parse(configText);
+const configValue = yaml.parse(configText);
+export default configValue;
 
 if (process.env.TESTING_MODE === 'TRUE') {
-	console.log('Config installed:\n' + configText);
+	console.log('Config installed:\n' + JSON.stringify(configValue));
 }
