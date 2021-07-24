6927ae604ff81f5a2ba63c1c831d0074cedad66d
Keyten <ikeyten@gmail.com>
2021-07-24 16:53:07 +1000

    Test

--- src/models/init-sequelize.ts
+++ src/models/init-sequelize.ts
@@ -2,6 +2,10 @@ import { Sequelize } from 'sequelize';
 import config from '../lib/config';
 import { databaseUrl } from '../lib/secret-config';
 
+console.log(databaseUrl, {
+	dialect: 'postgres',
+	...config.sequelizeConfig
+});
 export const sequelize = new Sequelize(databaseUrl, {
 	dialect: 'postgres',
 	...config.sequelizeConfig
